1312b5949f0da9c5407fd30919fa3f0b00454a78
Sam Sabat <samnsabat@gmail.com>
2024-07-03 21:26:12 +0200

    working on register form

--- src/views/register.view.php
+++ src/views/register.view.php
@@ -1,4 +1,51 @@
 <?php
+// si besoin de rediriger l'user, il ne faut pas de html avant. /!\
+// on vérifie que le formulaire a été envoyé :
+if (!empty($_POST)) {
+
+
+
+  // on vérifie que tous les champs sont remplis :
+  if (
+    isset($_POST["inputNom"], $_POST["inputPrenom"], $_POST["inputPseudo"], $_POST["inputEmail"], $_POST["inputPassword"], $_POST["inputPasswordConfirm"])
+    && !empty($_POST["inputNom"]) && !empty($_POST["inputPrenom"]) && !empty($_POST["inputPseudo"]) && !empty($_POST["inputEmail"])
+    && !empty($_POST["inputPassword"]) && !empty($_POST["inputPasswordConfirm"])
+  ) {
+    // form complet
+    // récupération des données du formulaire :
+    $nom = htmlspecialchars($_POST['inputNom']);
+    $prenom = htmlspecialchars($_POST['inputPrenom']);
+    $pseudo = htmlspecialchars($_POST['inputPseudo']);
+    $email = htmlspecialchars($_POST['inputEmail']);
+    $password = $_POST['inputPassword'];
+    $passwordConfirm = $_POST['inputPasswordConfirm'];
+    $motDePasse = password_hash($_POST['inputPassword'], PASSWORD_ARGON2ID);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      die("L'adresse email est incorrecte.");
+      // email valide
+      // vérification que le mot de passe est assez long et contient des lettres minuscules, majuscules, chiffres et des caractères spéciaux
+      // if (strlen($_POST['inputPassword']) >= 8 && preg_match('/[a-z]/', $_POST['inputPassword']) && preg_match('/[A-Z]/', $_POST['inputPassword']) && preg_match('/[0-9]/', $_POST['inputPassword']) && preg_match('/[^a-zA-Z0-9]/', $_POST['inputPassword'])) {
+        // mot de passe correct
+        // vérification que le mot de passe et sa confirmation sont identiques
+        // if ($_POST['inputPassword'] === $_POST['inputPasswordConfirm']) {
+          // mots de passe identiques
+          // création de l'utilisateur en base de données
+          
+    }
+
+  } else {
+    // form non complet
+    die("Le formulaire est incomplet.");
+  }
+}
+
+
+
+
+
+
+
 $titre = "Phone Way Inscription";
 require_once __DIR__ . "/templates/head.php"; ?>
 <div class="wrapper">
@@ -8,30 +55,30 @@ require_once __DIR__ . "/templates/head.php"; ?>
     <section class="container my-5 d-flex justify-content-center align-items-center">
       <div class="row loginFormStyle d-flex justify-content-center align-items-center">
         <h2 class="mb-4 text-center">Bienvenue chez Phone Way !</h2>
-        <form class="border border-2 rounded-3 p-3">
+        <form class="border border-2 rounded-3 p-3" method="POST" novalidate>
           <div class="mb-3">
             <label for="inputNom" class="form-label">Nom :</label>
-            <input type="text" class="form-control" id="inputNom" placeholder="Entrez votre nom" required>
+            <input type="text" class="form-control" id="inputNom" name="inputNom" placeholder="Entrez votre nom" required>
           </div>
           <div class="mb-3">
             <label for="inputPrenom" class="form-label">Prénom :</label>
-            <input type="text" class="form-control" id="inputPrenom" placeholder="Entrez votre prénom" required>
+            <input type="text" class="form-control" id="inputPrenom" name="inputPrenom" placeholder="Entrez votre prénom" required>
           </div>
           <div class="mb-3">
             <label for="inputPseudo" class="form-label">Pseudo :</label>
-            <input type="text" class="form-control" id="inputPseudo" placeholder="Entrez votre pseudo" required>
+            <input type="text" class="form-control" id="inputPseudo" name="inputPseudo" placeholder="Entrez votre pseudo" required>
           </div>
           <div class="mb-3">
             <label for="inputEmail" class="form-label">Adresse Email :</label>
-            <input type="email" class="form-control" id="inputEmail" placeholder="Entrez votre email" required>
+            <input type="email" class="form-control" id="inputEmail" name="inputEmail" placeholder="Entrez votre email" required>
           </div>
           <div class="mb-3">
             <label for="inputPassword" class="form-label">Mot de passe :</label>
-            <input type="password" class="form-control" id="inputPassword" placeholder="Entrez votre mot de passe" required>
+            <input type="password" class="form-control" id="inputPassword" name="inputPassword" placeholder="Entrez votre mot de passe" required>
           </div>
           <div class="mb-4">
             <label for="inputPasswordConfirm" class="form-label">Vérification du mot de passe :</label>
-            <input type="password" class="form-control" id="inputPasswordConfirm" placeholder="Confirmez votre mot de passe" required>
+            <input type="password" class="form-control" id="inputPasswordConfirm" name="inputPasswordConfirm" placeholder="Confirmez votre mot de passe" required>
           </div>
           <div class="d-flex justify-content-center">
             <button type="submit" class="btn btn-outline-dark btn-dark text-light">S'enregistrer</button>
--- src/views/templates/contact.php
+++ src/views/templates/contact.php
@@ -5,54 +5,29 @@ if (isset($_POST['send'])) {
   $email = htmlspecialchars($_POST['email']);
   $message = htmlspecialchars($_POST['message']);
 
-  // require_once 'C:/wamp64/www/PhoneWay_Sam/src/configs/db.config.php';
-  require_once __DIR__ . "/../../configs/db.config.php";
-  $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME;
-  $user = DB_USER;
-  $pass = DB_PASSWORD;
+  require_once __DIR__ . "/../../configs/connect.php";
 
-  // débogage du code suite à des erreurs de frappe
-  // echo "DSN: $dsn<br>";
   try {
-    $db = new PDO(
-      $dsn,
-      $user,
-      $pass,
-      array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-      )
-    );
-
-    $db = new PDO(
-      $dsn,
-      $user,
-      $pass,
-      array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-      )
-    );
     $sql = "INSERT INTO contact (fullname, email, message) VALUES (?, ?, ?);";
     // protection contre les injections SQL
     $stmt = $db->prepare($sql);
     $result = $stmt->execute([$fullname, $email, $message]);
     $result = $stmt->rowCount() == 1;
-  } catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-    exit;
-  }
-  if ($result) { ?>
-    <div class="alert alert-success alert dismissible fade show marginSendMsg" role="alert">
-      Votre message est envoyé.
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    <script>
-      if (history.replaceState) {
-        history.replaceState(null, null, location.href);
-      }
-    </script>
+
+    if ($result) { ?>
+      <div class="alert alert-success alert dismissible fade show marginSendMsg" role="alert">
+        Votre message est envoyé.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <script>
+        if (history.replaceState) {
+          history.replaceState(null, null, location.href);
+        }
+      </script>
 <?php }
+  } catch (PDOException $e) {
+    echo "Erreur lors de l'insertion : " . $e->getMessage();
+  }
 }
 ?>
 
@@ -65,18 +40,18 @@ if (isset($_POST['send'])) {
       </div>
 
       <div class="modal-body">
-        <form id="contactForm" method="POST" action="">
+        <form id="contactForm" method="POST" action="" novalidate>
           <div class="mb-3">
             <label class="form-label" for="fullname">Name</label>
-            <input class="form-control" id="fullname" name="fullname" type="text" placeholder="Name">
+            <input class="form-control" id="fullname" name="fullname" type="text" placeholder="Name" required>
           </div>
           <div class="mb-3">
             <label class="form-label" for="email"></label>
-            <input class="form-control" id="email" name="email" type="text" placeholder="Email Address" autocomplete="email">
+            <input class="form-control" id="email" name="email" type="text" placeholder="Email Address" autocomplete="email" required>
           </div>
           <div class="mb-3">
             <label class="form-label" for="message">Message</label>
-            <textarea class="form-control" id="message" name="message" type="text" placeholder="Message" style="height: 10rem;"></textarea>
+            <textarea class="form-control" id="message" name="message" type="text" placeholder="Message" style="height: 10rem;" required></textarea>
           </div>
           <div class="mb-1 text-center">
             <button class="btn btn-success" name="send" type="submit">Envoyer</button>
