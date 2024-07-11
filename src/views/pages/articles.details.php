459a52bf58b8d8af9b2ea281be048cd3d3da807d
Sam Sabat <samnsabat@gmail.com>
2024-07-11 17:40:15 +0200

    hotfix path

--- src/views/pages/articles.details.php
+++ src/views/pages/articles.details.php
@@ -7,20 +7,20 @@ require_once __DIR__ . "/../partials/head.php"; ?>
   <main class="mt-5 pt-3 row">
     <div class="col-12">
       <div class="text-center my-3">
-        <h3 class="mt-5"><?= $article["titre"] ?></h3>
+        <h3 class="mt-5"><?= $article->titre ?></h3>
         <img src=" https://picsum.photos/600/300?random=<?= rand(1, 200) ?>" class="mt-3" alt="Photo de présentation de l'article">
       </div>
 
       <div class="d-flex justify-content-center align-items-center flex-column">
         <p class="text-center my-3 mx-5 w-50">
-          <?= $article["description"] ?>
+          <?= $article->description ?>
         </p>
         <p class="text-center">
-          <i>Publié le <?= date("d/m/Y", strtotime($article["published_at"])); ?>
+          <i>Publié le <?= date("d/m/Y", strtotime($article->published_at)); ?>
             <?php
-            if (isset($article["updated_at"])) {
+            if (isset($article->updated_at)) {
             ?>
-              (Mis à jour le <?= date("d/m/Y", strtotime($article["updated_at"])); ?>)</i>
+              (Mis à jour le <?= date("d/m/Y", strtotime($article->updated_at)); ?>)</i>
         <?php
             }
         ?>
